665437aebf1b743753a28787b8d0a5649d2df499
fjpolo <fjpolo@gmail.com>
2025-02-22 19:54:05 +0100

    Add iverilog templates

---
+++ test/icarus/barrier_sync/run.sh
@@ -0,0 +1,35 @@
+# !/bin/bash
+
+# Environmental Variables
+RTL_PATH="../../../rtl"
+MODULE_NAME="barrier_sync"
+ORIGINAL_FILE="$RTL_PATH/${MODULE_NAME}.sv"
+ICARUS_TB_FILE="${MODULE_NAME}_tb.sv"
+ICARUS_OUTPUT_FILE="${MODULE_NAME}__out"
+SIMULATOR="iverilog"
+
+# Run simulation of slave_tb.v with the original slave.v
+echo "Running simulation for $MODULE_NAME..."
+cp $ORIGINAL_FILE .
+echo "${MODULE_NAME}.sv"
+$SIMULATOR -o ${ICARUS_OUTPUT_FILE} ${MODULE_NAME}.sv ${ICARUS_TB_FILE}
+if [ $? -ne 0 ]; then
+    echo "Simulation compilation failed for $MODULE_NAME. Exiting script."
+    rm -f ${ICARUS_OUTPUT_FILE}
+    rm -f ${MODULE_NAME}.sv
+    exit 1
+fi
+
+# Execute the simulation and check the exit status
+./${ICARUS_OUTPUT_FILE}
+SIM_EXIT_STATUS=$?
+if [ $SIM_EXIT_STATUS -ne 0 ]; then
+    echo "Simulation failed for $MODULE_NAME with exit status $SIM_EXIT_STATUS. Exiting script."
+    rm -f ${ICARUS_OUTPUT_FILE}
+    rm -f ${MODULE_NAME}.sv
+    exit 1
+fi
+rm -f ${MODULE_NAME}.sv
+
+# Clean up the simulation executable
+rm -f ${ICARUS_OUTPUT_FILE}
---
+++ test/icarus/shared_memory/shared_memory_tb.sv
@@ -0,0 +1,28 @@
+`timescale 1ns/1ps
+
+module shared_memory_tb;
+
+  parameter NUM_CORES = 1; // Test with 4 core
+
+  reg clk;
+  reg rst;
+
+  shared_memory #(NUM_CORES) uut ();
+
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk; // 10ns clock period
+  end
+
+  initial begin
+    rst = 1;
+    #10;
+    rst = 0;
+
+    // Pass
+    #10;
+    $display("PASS: All test cases passed!");
+    $finish;
+  end
+
+endmodule
