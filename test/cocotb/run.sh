a85a651807dc2a1200e2db2e567a098621099caa
fjpolo <fjpolo@gmail.com>
2025-02-22 19:20:16 +0100

    Source oss-cad-suite environment for cocoTB

--- test/cocotb/run.sh
+++ test/cocotb/run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# Create virtualenv with cocotb
-
+# Source the OSS CAD Suite environment
+echo "Sourcing OSS CAD Suite environment..."
+source ~/oss-cad-suite/environment
+if [ $? -ne 0 ]; then
+    echo "Failed to source OSS CAD Suite environment. Exiting script."
+    exit 1
+fi
 
 # Run the testbench using the Makefile
 python test_runner.py 
